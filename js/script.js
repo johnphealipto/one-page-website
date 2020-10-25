458cdea7396c38fb11fb25ad1a5b6285df6fe065
John-Phealipto <johnadibe450@gmail.com>
2020-10-25 01:51:30 +0100

    Carousel

--- js/script.js
+++ js/script.js
@@ -25,3 +25,28 @@ var offset = 300, // browser window scroll (in pixels) after which the "back to 
 	scrollDuration = 700;
 // // *** END OF GET THE SCROLL TOP BUTTON
 
+
+
+// // ==== CAROUSEL SETTING
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+    
+  for (var i=0;i<minPerSlide;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+        
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+// // ==== END CAROUSEL SETTINGS
